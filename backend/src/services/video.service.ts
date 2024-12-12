a7c5999724bb166b92076677b6ec63e657ff8a87
Thiago Bussola <thiagobussola@hotmail.com>
2024-12-12 17:19:18 -0300

    video-2.2

--- backend/src/services/video.service.ts
+++ backend/src/services/video.service.ts
@@ -3,6 +3,7 @@ import { ensureDirectoryExists, uploadDirectory } from "../../utils/helper";
 import path from "path";
 import { createWriteStream } from "fs";
 import { pipeline } from "stream";
+import { readdir } from "fs/promises";
 
 const videoRouter = Router();
 
@@ -24,6 +25,26 @@ videoRouter.post("/upload/:fileName", (req: Request, res: Response) => {
 
     res.status(200).send(`Upload realizado com sucesso: ${fileName}`);
   });
+});
+
+videoRouter.get("/videos", async (req: Request, res: Response) => {
+  ensureDirectoryExists(uploadDirectory);
+
+  const files = await readdir(uploadDirectory);
+  const videoFiles = files.filter((file) => {
+    const ext = path.extname(file).toLowerCase();
+    return [".mp4", ".avi", ".mov", ".mkv"].includes(ext);
+  });
+
+  const videos = videoFiles.map((file) => ({
+    url: `http://localhost:3001/video/${file}`,
+    titulo: file,
+    descricao: "Descrição do vídeo",
+    thumbnail: "http://localhost:3001/uploads/thumbnail.png",
+  }));
+
+  console.log(videos);
+  res.status(200).json(videos);
 });
 
 export { videoRouter };
