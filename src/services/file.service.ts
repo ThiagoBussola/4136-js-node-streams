b716b5f11d6124e92948c229e2b878c77024ca8c
Thiago Bussola <thiagobussola@hotmail.com>
2024-12-12 16:36:26 -0300

    video-1.3

--- src/services/file.service.ts
+++ src/services/file.service.ts
@@ -15,6 +15,16 @@ function readLargeFile() {
   });
 }
 
+function transformCsvLine(line: string) {
+  const parts = line.split(",");
+  if (parts.length === 3) {
+    parts[0] = parts[0].trim().toUpperCase();
+    const alterationDate = new Date().toISOString();
+    return [...parts, alterationDate].join(",") + "\n";
+  }
+  return line + "\n";
+}
+
 //brokenApp();
 
-readLargeFile();
+//readLargeFile();
